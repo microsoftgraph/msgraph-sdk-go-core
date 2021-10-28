af46a233f9fd12b552f2562f0b3e19be7023f0ac
Vincent Biret <vibiret@microsoft.com>
2021-10-28 13:25:24 -0400

    - adds options to the odata query middleware to support disabling it per request

--- graph_odata_query_handler.go
+++ graph_odata_query_handler.go
@@ -4,26 +4,63 @@ import (
 	nethttp "net/http"
 	regexp "regexp"
 
+	abs "github.com/microsoft/kiota/abstractions/go"
 	khttp "github.com/microsoft/kiota/http/go/nethttp"
 )
 
 // GraphODataQueryHandler is a handler that adds the dollar sign in front of OData query parameters
 type GraphODataQueryHandler struct {
-	regex *regexp.Regexp
+	regex          *regexp.Regexp
+	handlerOptions GraphODataQueryHandlerOptions
+}
+
+type GraphODataQueryHandlerOptions struct {
+	shouldReplace func(*nethttp.Request) bool
+}
+
+type graphODataQueryHandlerOptionsInt interface {
+	abs.RequestOption
+	ShouldReplace(*nethttp.Request) bool
+}
+
+func (o *GraphODataQueryHandlerOptions) ShouldReplace(req *nethttp.Request) bool {
+	return o.shouldReplace(req)
+}
+
+var keyValue = abs.RequestOptionKey{
+	Key: "GraphODataQueryHandler",
+}
+
+func (o *GraphODataQueryHandlerOptions) GetKey() abs.RequestOptionKey {
+	return keyValue
 }
 
 // NewGraphODataQueryHandler creates a new instance of GraphODataQueryHandler
 func NewGraphODataQueryHandler() *GraphODataQueryHandler {
-	//TODO constructor with options
+	return NewGraphODataQueryHandlerWithOptions(
+		GraphODataQueryHandlerOptions{
+			shouldReplace: func(*nethttp.Request) bool {
+				return true
+			},
+		})
+}
+
+// NewGraphODataQueryHandlerWithOptions creates a new instance of GraphODataQueryHandler
+// Parameters:
+// 		options: GraphODataQueryHandlerOptions options to use for the handler
+func NewGraphODataQueryHandlerWithOptions(options GraphODataQueryHandlerOptions) *GraphODataQueryHandler {
 	replacementRegexp := "(?i)([^$])(count|expand|filter|format|orderby|search|select|skip|skiptoken|top)="
 	return &GraphODataQueryHandler{
-		regex: regexp.MustCompile(replacementRegexp),
+		regex:          regexp.MustCompile(replacementRegexp),
+		handlerOptions: options,
 	}
 }
 
 func (middleware GraphODataQueryHandler) Intercept(pipeline khttp.Pipeline, req *nethttp.Request) (*nethttp.Response, error) {
-	//TODO get options from context, default back to properties options
-	req.URL.RawQuery = middleware.regex.ReplaceAllString("?"+req.URL.RawQuery, "$1$$$2=")[1:]
-	// inserting and removing the ? sign so we can make no dollar mandatory and avoid adding a second dollar when already here
+	reqOption, ok := req.Context().Value(keyValue).(graphODataQueryHandlerOptionsInt)
+	if ok && reqOption.ShouldReplace(req) || !ok && middleware.handlerOptions.shouldReplace(req) {
+		req.URL.RawQuery = middleware.regex.ReplaceAllString("?"+req.URL.RawQuery, "$1$$$2=")[1:]
+		// inserting and removing the ? sign so we can make no dollar mandatory and avoid adding a second dollar when already here
+	}
 	return pipeline.Next(req)
 }
--- graph_odata_query_handler_test.go
+++ graph_odata_query_handler_test.go
@@ -3,8 +3,12 @@ package msgraphgocore
 import (
 	nethttp "net/http"
 	httptest "net/http/httptest"
+	"net/url"
+	"strings"
 	testing "testing"
 
+	abs "github.com/microsoft/kiota/abstractions/go"
+	absauth "github.com/microsoft/kiota/abstractions/go/authentication"
 	assert "github.com/stretchr/testify/assert"
 )
 
@@ -28,4 +32,34 @@ func TestItReplacesQueryParameters(t *testing.T) {
 	assert.Equal(t, "something", query.Get("$Select"))
 	assert.Equal(t, "somethingElse($select=nested)", query.Get("$exPand"))
 	assert.Equal(t, "10", query.Get("$top"))
+}
+
+func TestItDoesNotReplaceWithLocalQueryOptions(t *testing.T) {
+	testServer := httptest.NewServer(nethttp.HandlerFunc(func(res nethttp.ResponseWriter, req *nethttp.Request) {
+		if strings.Contains(req.URL.RawQuery, "$") {
+			t.Error("Query parameter $ was added")
+		}
+		res.WriteHeader(200)
+		res.Write([]byte("body"))
+	}))
+	defer func() { testServer.Close() }()
+	auth := &absauth.AnonymousAuthenticationProvider{}
+	requestAdapter, err := NewGraphRequestAdapterBase(auth, GraphClientOptions{
+		GraphServiceVersion:        "na",
+		GraphServiceLibraryVersion: "na",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	absRequest := abs.NewRequestInformation()
+	targetUrl, err := url.Parse(testServer.URL + "/?Select=something")
+	if err != nil {
+		t.Error(err)
+	}
+	absRequest.SetUri(*targetUrl)
+	absRequest.Method = abs.GET
+	absRequest.AddRequestOptions(&GraphODataQueryHandlerOptions{
+		shouldReplace: func(*nethttp.Request) bool { return false },
+	})
+	requestAdapter.SendNoContentAsync(*absRequest, nil)
 }

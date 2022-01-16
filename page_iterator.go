5481491742d8b9b0fd8458e59ad47b060512c385
jobala <japhethobalak@gmail.com>
2022-01-16 22:32:12 +0300

    adds comments

--- page_iterator.go
+++ page_iterator.go
@@ -5,6 +5,7 @@ import "net/http"
 type Item interface {
 	GetDisplayName() string
 }
+
 type Page interface {
 	GetValue() []Item
 	GetNextLink() string
@@ -12,18 +13,18 @@ type Page interface {
 }
 
 type PageIterator struct {
-	page          Page
-	client        http.Client
-	keepIterating bool
-	pauseIndex    int
+	page       Page
+	client     http.Client
+	pauseIndex int
 }
 
 func NewPageIterator(page Page, client http.Client) *PageIterator {
+	//TODO: Pass client to page
+
 	return &PageIterator{
 		page,
 		client,
-		true,
-		0,
+		0, // pauseIndex helps us remember where we paused enumeration in the page.
 	}
 }
 
@@ -46,11 +47,12 @@ func (pI *PageIterator) Iterate(callback func(pageItem Item) bool) {
 		keepIterating := pI.enumerate(callback)
 
 		if !keepIterating {
+			// Callback returned false, stop iterating through pages.
 			return
 		}
 
 		pI.Next()
-		pI.pauseIndex = 0
+		pI.pauseIndex = 0 // when moving to the next page reset pauseIndex
 	}
 }
 
@@ -58,10 +60,14 @@ func (pI *PageIterator) enumerate(callback func(item Item) bool) bool {
 	keepIterating := true
 	pageItems := pI.page.GetValue()
 
+	// start/continue enumerating page items from  pauseIndex.
+	// this makes it possible to resume iteration from where we paused iteration.
 	for i := pI.pauseIndex; i < len(pageItems); i++ {
 		keepIterating = callback(pageItems[i])
 
 		if !keepIterating {
+			// Callback returned false, pause! stop enumerating page items. Set pauseIndex so that we know
+			// where to resume from.
 			pI.pauseIndex = i
 			break
 		}

526a5e00303f7d7d8ec07037ad5ab9aad8842dc7
jobala <japhethobalak@gmail.com>
2022-01-22 16:15:09 +0300

    updates tests

--- page_iterator_test.go
+++ page_iterator_test.go
@@ -1,8 +1,12 @@
 package msgraphgocore
 
 import (
+	"fmt"
+	nethttp "net/http"
+	httptest "net/http/httptest"
 	testing "testing"
 
+	"github.com/microsoft/kiota/abstractions/go/authentication"
 	"github.com/microsoft/kiota/abstractions/go/serialization"
 	"github.com/microsoftgraph/msgraph-sdk-go-core/msgraphgocore_test"
 	"github.com/stretchr/testify/assert"
@@ -17,117 +21,140 @@ type UserPage struct {
 	NextLink *string
 }
 
-var page2Link = "page2"
-var page3Link = "page3"
-
-var page1 = &UserPage{
-	Value: []interface{}{
-		PageItem{DisplayName: "User A"},
-		PageItem{DisplayName: "User B"},
-		PageItem{DisplayName: "User C"},
-		PageItem{DisplayName: "User D"},
-	},
-	NextLink: &page2Link,
-}
-
-var page2 = &UserPage{
-	Value: []interface{}{
-		PageItem{DisplayName: "User E"},
-		PageItem{DisplayName: "User F"},
-		PageItem{DisplayName: "User G"},
-		PageItem{DisplayName: "User H"},
-	},
-	NextLink: &page3Link,
-}
-
-var page3 = &UserPage{
-	Value: []interface{}{
-		PageItem{DisplayName: "User I"},
-		PageItem{DisplayName: "User J"},
-		PageItem{DisplayName: "User K"},
-		PageItem{DisplayName: "User L"},
-	},
-	NextLink: nil,
-}
-
-var reqAdapter = &GraphRequestAdapterBase{}
+var reqAdapter, _ = NewGraphRequestAdapterBase(&authentication.AnonymousAuthenticationProvider{}, GraphClientOptions{
+	GraphServiceVersion:        "",
+	GraphServiceLibraryVersion: "",
+})
 
 func ParsableCons() serialization.Parsable {
 	return msgraphgocore_test.NewUsersResponse()
 }
 
-var res = msgraphgocore_test.NewUsersResponse()
-
-func TestHasNextReturnsTrueIfNextPageIsAvailable(t *testing.T) {
-	pageIterator := NewPageIterator(res, *reqAdapter, ParsableCons)
-	hasNext := pageIterator.HasNext()
-	assert.True(t, hasNext)
-}
-
-func TestNextReturnsNextPage(t *testing.T) {
-	pageIterator := NewPageIterator(res, *reqAdapter, ParsableCons)
-	nextPage := pageIterator.Next()
-
-	assert.Equal(t, nextPage, page2)
-}
-
 func TestIterateStopsWhenCallbackReturnsFalse(t *testing.T) {
 	res := make([]string, 0)
+	graphResponse := buildGraphResponse()
+	testServer := httptest.NewServer(nethttp.HandlerFunc(func(w nethttp.ResponseWriter, req *nethttp.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, `
+			{
+				"nextLink": "",
+				"value": [
+	        		{
+	            		"id": "10"
+	        		}
+	        	]
+        	}
+        `)
 
-	pageIterator := NewPageIterator(res, *reqAdapter, ParsableCons)
+	}))
+	defer testServer.Close()
+	pageIterator := NewPageIterator(graphResponse, *reqAdapter, ParsableCons)
+
 	pageIterator.Iterate(func(pageItem interface{}) bool {
 		item := pageItem.(msgraphgocore_test.User)
 
 		res = append(res, *item.GetDisplayName())
-		return *item.GetDisplayName() == "User D"
+		return !(*item.GetId() == "2")
 	})
 
-	assert.Equal(t, 4, len(res))
+	assert.Equal(t, len(res), 3)
 }
 
-func TestNextAndHasNext(t *testing.T) {
-	pageCount := 1
-	pageIterator := NewPageIterator(res, *reqAdapter, ParsableCons)
-
-	for pageIterator.HasNext() {
-		pageCount += 1
-		_ = pageIterator.Next()
-	}
-
-	assert.Equal(t, pageCount, 3)
-}
 func TestIterateEnumeratesAllPages(t *testing.T) {
-	pageIterator := NewPageIterator(res, *reqAdapter, ParsableCons)
+	testServer := httptest.NewServer(nethttp.HandlerFunc(func(w nethttp.ResponseWriter, req *nethttp.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, `
+			{
+				"nextLink": "",
+				"value": [
+	        		{
+	            		"id": "10"
+	        		}
+	        	]
+        	}
+        `)
+
+	}))
+	defer testServer.Close()
+
+	graphResponse := buildGraphResponse()
+	mockPath := testServer.URL + "/next-page"
+	graphResponse.SetNextLink(&mockPath)
+
+	pageIterator := NewPageIterator(graphResponse, *reqAdapter, ParsableCons)
 	res := make([]string, 0)
 
 	pageIterator.Iterate(func(pageItem interface{}) bool {
 		item := pageItem.(msgraphgocore_test.User)
-		res = append(res, *item.GetDisplayName())
+		res = append(res, *item.GetId())
 		return true
 	})
 
-	assert.Equal(t, 12, len(res))
+	// Initial page has 5 items and the next page has 1 item.
+	assert.Equal(t, len(res), 6)
 }
 
 func TestIterateCanBePausedAndResumed(t *testing.T) {
 	res := make([]string, 0)
 	res2 := make([]string, 0)
-	pageIterator := NewPageIterator(res, *reqAdapter, ParsableCons)
+
+	testServer := httptest.NewServer(nethttp.HandlerFunc(func(w nethttp.ResponseWriter, req *nethttp.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, `
+			{
+				"nextLink": "",
+				"value": [
+	        		{
+	            		"id": "10"
+	        		}
+	        	]
+        	}
+        `)
+
+	}))
+	defer testServer.Close()
+
+	response := buildGraphResponse()
+	mockPath := testServer.URL + "/next-page"
+	response.SetNextLink(&mockPath)
+
+	pageIterator := NewPageIterator(response, *reqAdapter, ParsableCons)
+	pageIterator.Iterate(func(pageItem interface{}) bool {
+		item := pageItem.(msgraphgocore_test.User)
+		res = append(res, *item.GetId())
+
+		if *item.GetId() == "2" {
+			return false
+		}
+		return true
+	})
+	assert.Equal(t, res, []string{"0", "1", "2"})
 
 	pageIterator.Iterate(func(pageItem interface{}) bool {
 		item := pageItem.(msgraphgocore_test.User)
+		res2 = append(res2, *item.GetId())
 
-		res = append(res, *item.GetDisplayName())
-
-		return *item.GetDisplayName() == "User D"
+		return true
 	})
-	assert.Equal(t, res, []string{"User A", "User B", "User C", "User D"})
+	assert.Equal(t, res2, []string{"2", "3", "4", "10"})
+}
 
-	pageIterator.Iterate(func(pageItem interface{}) bool {
-		item := pageItem.(msgraphgocore_test.User)
-		res2 = append(res2, *item.GetDisplayName())
+func buildGraphResponse() *msgraphgocore_test.UsersResponse {
+	var res = msgraphgocore_test.NewUsersResponse()
 
-		return *item.GetDisplayName() == "User G"
-	})
-	assert.Equal(t, res2, []string{"User D", "User E", "User F", "User G"})
+	nextLink := "next-page"
+	users := make([]msgraphgocore_test.User, 0)
+
+	for i := 0; i < 5; i++ {
+		u := msgraphgocore_test.NewUser()
+		id := fmt.Sprint(i)
+		u.SetId(&id)
+
+		users = append(users, *u)
+	}
+
+	res.SetNextLink(&nextLink)
+	res.SetValue(users)
+
+	return res
 }

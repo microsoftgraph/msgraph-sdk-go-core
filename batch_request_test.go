6d0adee89ffd0c71ff0a3ce1ff7bba0be0525fa9
jobala <japhethobalak@gmail.com>
2022-07-13 19:33:59 +0100

    fix failing test and add test for dependsOn relationship

--- batch_request_test.go
+++ batch_request_test.go
@@ -2,44 +2,43 @@ package msgraphgocore
 
 import (
 	"net/url"
-	"strings"
 	"testing"
 
 	abstractions "github.com/microsoft/kiota-abstractions-go"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestJSONBody(t *testing.T) {
+func TestGeneratesJSONFromRequestBody(t *testing.T) {
 	reqInfo := getRequestInfo()
 
 	batch := NewBatchRequest()
 	item, _ := batch.AppendItem(*reqInfo)
 	item.Id = "1"
 
-	expected := `
-    {
-        "requests": [
-            {
-                "method": "GET",
-                "url": "",
-                "body": {
-                    "username": "name"
-                },
-                "headers": {
-                    "content-type": "application/json"
-                }
-            }
-        ]
-    }
-    `
+	expected := "{\"requests\":[{\"id\":\"1\",\"method\":\"GET\",\"url\":\"\",\"headers\":{\"content-type\":\"application/json\"},\"body\":{\"username\":\"name\"},\"dependsOn\":[]}]}"
 	actual, _ := batch.toJson()
 
-	expectedWithoutNewLine := strings.Replace(expected, "\n", "", -1)
-	expectedWithoutSpace := strings.Replace(expectedWithoutNewLine, " ", "", -1)
-
-	assert.Equal(t, expectedWithoutSpace, string(actual))
+	assert.Equal(t, expected, string(actual))
 }
-func TestDependsOnRelationship(t *testing.T) {}
+
+func TestDependsOnRelationshipInBatchRequestItems(t *testing.T) {
+
+	reqInfo1 := getRequestInfo()
+	reqInfo2 := getRequestInfo()
+
+	batch := NewBatchRequest()
+	batchItem1, _ := batch.AppendItem(*reqInfo1)
+	batchItem2, _ := batch.AppendItem(*reqInfo2)
+	batchItem1.Id = "1"
+	batchItem2.Id = "2"
+
+	batchItem2.DependsOnItem(*batchItem1)
+
+	expected := "{\"requests\":[{\"id\":\"1\",\"method\":\"GET\",\"url\":\"\",\"headers\":{\"content-type\":\"application/json\"},\"body\":{\"username\":\"name\"},\"dependsOn\":[]},{\"id\":\"2\",\"method\":\"GET\",\"url\":\"\",\"headers\":{\"content-type\":\"application/json\"},\"body\":{\"username\":\"name\"},\"dependsOn\":[\"1\"]}]}"
+	actual, _ := batch.toJson()
+
+	assert.Equal(t, expected, string(actual))
+}
 
 func getRequestInfo() *abstractions.RequestInformation {
 	content := `

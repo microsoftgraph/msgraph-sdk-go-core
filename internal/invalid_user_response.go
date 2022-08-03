de9feba19d4f576abb10df1eb00f10b7db0618e8
Andrew Omondi <andrew.omondi@microsoft.com>
2022-08-03 10:01:32 +0300

    Remove dead duplicated code

--- internal/invalid_user_response.go
+++ internal/invalid_user_response.go
@@ -1,9 +1,5 @@
 package internal
 
-import (
-	i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55 "github.com/microsoft/kiota-abstractions-go/serialization"
-)
-
 // InvalidUsersResponse
 type InvalidUsersResponse struct {
 	// Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
@@ -48,67 +44,8 @@ func (m *InvalidUsersResponse) GetValue() []User {
 	}
 }
 
-// GetFieldDeserializers the deserialization information for the current model
-func (m *InvalidUsersResponse) GetFieldDeserializers() map[string]func(i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.ParseNode) error {
-	res := make(map[string]func(i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.ParseNode) error)
-	res["@odata.nextLink"] = func(n i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.ParseNode) error {
-		val, err := n.GetStringValue()
-		if err != nil {
-			return err
-		}
-		if val != nil {
-			m.SetNextLink(val)
-		}
-		return nil
-	}
-	res["value"] = func(n i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.ParseNode) error {
-		val, err := n.GetCollectionOfObjectValues(func(pn i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.ParseNode) (i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.Parsable, error) {
-			return NewUser(), nil
-		})
-		if err != nil {
-			return err
-		}
-		if val != nil {
-			res := make([]User, len(val))
-			for i, v := range val {
-				res[i] = *(v.(*User))
-			}
-			m.SetValue(res)
-		}
-		return nil
-	}
-	return res
-}
 func (m *InvalidUsersResponse) IsNil() bool {
 	return m == nil
-}
-
-// Serialize serializes information the current object
-func (m *InvalidUsersResponse) Serialize(writer i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.SerializationWriter) error {
-	{
-		err := writer.WriteStringValue("@odata.nextLink", m.GetNextLink())
-		if err != nil {
-			return err
-		}
-	}
-	{
-		cast := make([]i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.Parsable, len(m.GetValue()))
-		for i, v := range m.GetValue() {
-			temp := v
-			cast[i] = i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.Parsable(&temp)
-		}
-		err := writer.WriteCollectionOfObjectValues("value", cast)
-		if err != nil {
-			return err
-		}
-	}
-	{
-		err := writer.WriteAdditionalData(m.GetAdditionalData())
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 // SetAdditionalData sets the additionalData property value. Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
